5eb9ad761cd8c9b785217edab9b43faa49410614
SachekDenis <sachek.denis@gmail.com>
2019-11-30 17:58:56 +0300

    minor fixes, fixed and added comments

--- Task2/PolinomialTest/PolinomialTest.cs
+++ Task2/PolinomialTest/PolinomialTest.cs
@@ -4,9 +4,15 @@ using Polinmial;
 
 namespace PolinomialTest
 {
+    /// <summary>
+    /// Tests of polinomial class
+    /// </summary>
     [TestClass]
     public class PolinomialTest
     {
+        /// <summary>
+        /// Testing creating of polinomial from valid data
+        /// </summary>
         [TestMethod]
         public void CreatingPolinomial()
         {
@@ -15,12 +21,19 @@ namespace PolinomialTest
             Assert.AreEqual(expectedDegree, polinomial.Degree);
         }
 
+        /// <summary>
+        /// Testing creating of polinomial from empty array
+        /// </summary>
         [TestMethod]
         public void CreatingPolinomialFromEmptyArrayMustThrowExeption()
         {
             Assert.ThrowsException<ArgumentNullException>(() => new Polinomial(new double[] { }));
         }
 
+
+        /// <summary>
+        /// Testing сorrectness of calculating sum of polinomials
+        /// </summary>
         [TestMethod]
         public void SumOfTwoPolinomialsNustReturnExpectedResult()
         {
@@ -34,6 +47,10 @@ namespace PolinomialTest
             Assert.AreEqual(expectedSumOfPolinomials, sumOfPolinomials);
         }
 
+
+        /// <summary>
+        /// Testing сorrectness of calculating difference of polinomials
+        /// </summary>
         [TestMethod]
         public void DifferenceOfTwoPolinomialsNustReturnExpectedResult()
         {
@@ -47,6 +64,9 @@ namespace PolinomialTest
             Assert.AreEqual(expectedSumOfPolinomials, sumOfPolinomials);
         }
 
+        /// <summary>
+        /// Testing сorrectness of calculating multiplying on number
+        /// </summary>
         [TestMethod]
         public void MultiplyingPolinomialsByNumberMustReturnValidResult()
         {
@@ -58,8 +78,41 @@ namespace PolinomialTest
             Assert.AreEqual(expectedPolimomial, polimomial);
         }
 
+
+        /// <summary>
+        /// Testing сorrectness of calculating multiplying on zero
+        /// </summary>
         [TestMethod]
-        public void MultiplayingOfTwoPolinomialsMustReturnValidResult()
+        public void MultiplyingPolinomialsByZeroNumberMustReturnValidResult()
+        {
+            Polinomial polimomial = new Polinomial(new double[] { 5, -3, 0, 9 });
+            int number = 0;
+            polimomial *= number;
+
+            var expectedPolimomial = new Polinomial(new double[] { 0, 0, 0, 0 });
+            Assert.AreEqual(expectedPolimomial, polimomial);
+        }
+
+        /// <summary>
+        /// Testing сorrectness of calculating multiplying on negative number
+        /// </summary>
+        [TestMethod]
+        public void MultiplyingPolinomialsByZNegativeNumberMustReturnValidResult()
+        {
+            Polinomial polimomial = new Polinomial(new double[] { 5, -3, 0, 9 });
+            int number = -3;
+            polimomial *= number;
+
+            var expectedPolimomial = new Polinomial(new double[] { -15, 9, 0, -27 });
+            Assert.AreEqual(expectedPolimomial, polimomial);
+        }
+
+
+        /// <summary>
+        /// Testing сorrectness of calculating multiplying of polinomilas with different size
+        /// </summary>
+        [TestMethod]
+        public void MultiplayingOfTwoPolinomialsDifferentSizeMustReturnValidResult()
         {
             Polinomial firstPolinomial = new Polinomial(new double[] { 5, -3, 0, 9 });
             Polinomial secondPolinomial = new Polinomial(new double[] { 11, 6, -12 });
@@ -70,6 +123,26 @@ namespace PolinomialTest
             Assert.AreEqual(expectedPolinomial, multiplayingOfTwoPolinomials);
         }
 
+
+        /// <summary>
+        /// Testing сorrectness of calculating multiplying of polinomilas with same size
+        /// </summary>
+        [TestMethod]
+        public void MultiplayingOfTwoPolinomialsSameSizeMustReturnValidResult()
+        {
+            Polinomial firstPolinomial = new Polinomial(new double[] { 5, -3, 0, 9 });
+            Polinomial secondPolinomial = new Polinomial(new double[] { 11, 6, -12, 3 });
+            var multiplayingOfTwoPolinomials = firstPolinomial * secondPolinomial;
+
+            var expectedPolinomial = new Polinomial(new double[] { 55, -3, -78, 150, 45, -108, 27 });
+
+            Assert.AreEqual(expectedPolinomial, multiplayingOfTwoPolinomials);
+        }
+
+
+        /// <summary>
+        /// Testing сorrectness of calculating dividing of polinomilas
+        /// </summary>
         [TestMethod]
         public void DividingOfTwoPolinomialsMustReturnValidResult()
         {
@@ -82,8 +155,28 @@ namespace PolinomialTest
             Assert.AreEqual(expectedPolinomial, dividingOfTwoPolinomials);
         }
 
+
+        /// <summary>
+        /// Testing сorrectness of calculating dividing of polinomial on zero polimial
+        /// </summary>
         [TestMethod]
-        public void DividingPolinomialWithSmallerDegreeOnPolinomialWithBiggerDegreeMustReturnValidResult()
+        public void DividingPolinomialOnZeroPolinomialMustReturnNull()
+        {
+            Polinomial firstPolinomial = new Polinomial(new double[] {-1, 0, -7, 0, 10 });
+            Polinomial secondPolinomial = new Polinomial(new double[] { 0 });
+            Polinomial dividingOfTwoPolinomials = firstPolinomial / secondPolinomial;
+
+            Polinomial expectedPolinomial = null;
+
+            Assert.AreEqual(expectedPolinomial, dividingOfTwoPolinomials);
+        }
+
+
+        /// <summary>
+        /// Testing сorrectness of calculating dividing of polinomial on smaller polinomial
+        /// </summary>
+        [TestMethod]
+        public void DividingPolinomialWithSmallerDegreeOnPolinomialWithBiggerDegreeMustReturnNull()
         {
             Polinomial firstPolinomial = new Polinomial(new double[] { 3, -1, 1 });
             Polinomial secondPolinomial = new Polinomial(new double[] {-1, 0, -7, 0, 10 });
--- Task2/VectorTest/VectorTest.cs
+++ Task2/VectorTest/VectorTest.cs
@@ -4,20 +4,31 @@ using VectorProject;
 
 namespace VectorTest
 {
+    /// <summary>
+    /// Tests for Vector class
+    /// </summary>
     [TestClass]
     public class VectorTest
     {
+        /// <summary>
+        /// Testing сorrectness of calculating sum of vectors
+        /// </summary>
         [TestMethod]
         public void SumOfTwoVectorsMustReturnNewVectorOfSum()
         {
             Vector firstVector = new Vector(new double[] { 1, 2, 3 });
             Vector secondVector = new Vector(new double[] { 4, 5, 6 });
-            var sumOfVectors = firstVector + secondVector;
 
+            var sumOfVectors = firstVector + secondVector;
             var expectedVector = new Vector(new double[] { 5, 7, 9 });
+
             Assert.AreEqual(expectedVector, sumOfVectors);
         }
 
+
+        /// <summary>
+        /// Testing сorrectness of calculating multiplying vector on number
+        /// </summary>
         [TestMethod]
         public void MultiplyingVectorByNumberMustReturnValidResult()
         {
@@ -29,6 +40,9 @@ namespace VectorTest
             Assert.AreEqual(expectedVector, secondVector);
         }
 
+        /// <summary>
+        /// Testing сorrectness of calculating multiplying of vectors
+        /// </summary>
         [TestMethod]
         public void MultiplayingOfTwoVectorsMustReturnValidResult()
         {
@@ -40,29 +54,84 @@ namespace VectorTest
             Assert.AreEqual(expectedResult, multiplayingOfTwoVectors);
         }
 
+
+        /// <summary>
+        /// Testing сorrectness of calculating difference of vectors
+        /// </summary>
         [TestMethod]
         public void DifferenceOfTwoVectorsMustReturnValidResult()
         {
             Vector firstVector = new Vector(new double[] { 5, 2, 1 });
             Vector secondVector = new Vector(new double[] { 4, 5, 6 });
-            var differenceOfTwoVectors = firstVector - secondVector;
 
+            var differenceOfTwoVectors = firstVector - secondVector;
             var expectedVector = new Vector(new double[] { 1, -3, -5 });
+
             Assert.AreEqual(expectedVector, differenceOfTwoVectors);
         }
 
+
+        /// <summary>
+        /// Testing сorrectness of calculating division of vectors
+        /// </summary>
         [TestMethod]
-        public void CreatingVectorFromArrayOfWrongSizeMustReturnExeption()
+        public void DivisionOfVectorsOnNumverMustReturnValidResult()
+        {
+            Vector firstVector = new Vector(new double[] { 5, 2, 1 });
+
+            var division = firstVector / 5;
+
+            var expectedVector = new Vector(new double[] { 1, 2.0/5, 1.0/5 });
+            Assert.AreEqual(expectedVector, division);
+        }
+
+        /// <summary>
+        /// Testing creating of polinomial from invalid data
+        /// </summary>
+        [TestMethod]
+        public void CreatingVectorFromArrayOfBiggerSizeMustReturnExeption()
         {
             Assert.ThrowsException<IndexOutOfRangeException>(()=> new Vector(new double[]{1,2,3,4}));
         }
 
+        /// <summary>
+        /// Testing creating of polinomial from invalid data
+        /// </summary>
+        [TestMethod]
+        public void CreatingVectorFromArrayOfSmallerSizeMustReturnExeption()
+        {
+            Assert.ThrowsException<IndexOutOfRangeException>(()=> new Vector(new double[]{1,2}));
+        }
+
+        /// <summary>
+        /// Testing creating of polinomial from invalid data
+        /// </summary>
+        [TestMethod]
+        public void CreatingVectorFromNullReturnExeption()
+        {
+            Assert.ThrowsException<ArgumentNullException>(()=> new Vector(null));
+        }
+
+        /// <summary>
+        /// Test equals of vectors
+        /// </summary>
         [TestMethod]
         public void ComparingOfTwoEqualVectorsMustReturnTrue()
         {
             Vector firstVector = new Vector(new double[] { 1, 2, 3 });
             Vector secondVector = new Vector(new double[] { 1, 2, 3 });
             Assert.IsTrue(firstVector == secondVector);
+        }
+
+        /// <summary>
+        /// Test equals of vectors
+        /// </summary>
+        [TestMethod]
+        public void ComparingOfTwoInequalVectorsMustReturnFalse()
+        {
+            Vector firstVector = new Vector(new double[] { 1, 2, 3 });
+            Vector secondVector = new Vector(new double[] { 1, 4, 3 });
+            Assert.IsFalse(firstVector == secondVector);
         }
     }
 }
