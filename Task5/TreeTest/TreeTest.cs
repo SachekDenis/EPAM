7e6f74d3570ff3982fc2c1a371fd97a27c665eec
SachekDenis <sachek.denis@gmail.com>
2019-12-27 00:10:38 +0300

    added more tests

--- Task5/TreeTest/TreeTest.cs
+++ Task5/TreeTest/TreeTest.cs
@@ -21,9 +21,10 @@ namespace TreeTestLibriary
         }
 
         [TestMethod]
-        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 2, new int[] { 3,1,4,5 })]
-        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 1, new int[] { 4,2,3,5 })]
-        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 4, new int[] { 2,1,5,3 })]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 2, new int[] { 3, 1, 4, 5 })]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 1, new int[] { 4, 2, 3, 5 })]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 4, new int[] { 2, 1, 5, 3 })]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 0, new int[] { 2, 1, 4, 3, 5 })]
         public void TreeDeleteTest(int[] arrayOfValues, int deleteValue, int[] expectedTree)
         {
             Tree<int> tree = new Tree<int>();
@@ -36,31 +37,102 @@ namespace TreeTestLibriary
         }
 
         [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 2)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 4)]
+        public void TreeFindTest(int[] arrayOfValues, int findValue)
+        {
+            Tree<int> tree = new Tree<int>();
+            Array.ForEach(arrayOfValues, element => tree.Add(element));
+            var findedValue = tree.Find(findValue);
+            Assert.AreEqual(findValue, findedValue);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 19)]
+        public void TreeFindUnexestedValueTestMustReturnDefaultResult(int[] arrayOfValues, int findValue)
+        {
+            Tree<int> tree = new Tree<int>();
+            Array.ForEach(arrayOfValues, element => tree.Add(element));
+            var findedValue = tree.Find(findValue);
+            Assert.AreEqual(0, findedValue);
+        }
+
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 2)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 4)]
+        public void TreeContainsTestMustReturnDefaultTrue(int[] arrayOfValues, int findValue)
+        {
+            Tree<int> tree = new Tree<int>();
+            Array.ForEach(arrayOfValues, element => tree.Add(element));
+            var isContains = tree.Contains(findValue);
+            Assert.IsTrue(isContains);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 19)]
+        public void TreeContainsUnexestedValueTestMustReturnDefaultFalse(int[] arrayOfValues, int findValue)
+        {
+            Tree<int> tree = new Tree<int>();
+            Array.ForEach(arrayOfValues, element => tree.Add(element));
+            var isContains = tree.Contains(findValue);
+            Assert.IsFalse(isContains);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 })]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [DataRow(new int[] { })]
+        public void TreeCountTestWithAdd(int[] arrayOfValues)
+        {
+            Tree<int> tree = new Tree<int>();
+            Array.ForEach(arrayOfValues, element => tree.Add(element));
+            var treeArray = tree.ToArray();
+
+            Assert.AreEqual(arrayOfValues.Count(), tree.Count);
+        }
+
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2 }, 3)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, new int[] { 7 }, 7)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, new int[] { 0 }, 8)]
+        public void TreeCountTestWithAddAndDelete(int[] arrayOfValues, int[] deleteValues, int expectedCount)
+        {
+            Tree<int> tree = new Tree<int>();
+            Array.ForEach(arrayOfValues, element => tree.Add(element));
+            Array.ForEach(deleteValues, element => tree.Delete(element));
+            var treeArray = tree.ToArray();
+
+            Assert.AreEqual(expectedCount, tree.Count);
+        }
+
+        [TestMethod]
         public void TreeAddNullMustThrowException()
         {
             Tree<string> tree = new Tree<string>();
-            Assert.ThrowsException<ArgumentNullException>(()=>tree.Add(null));
+            Assert.ThrowsException<ArgumentNullException>(() => tree.Add(null));
         }
 
         [TestMethod]
         public void TreeDeleteNullMustThrowException()
         {
             Tree<string> tree = new Tree<string>();
-            Assert.ThrowsException<ArgumentNullException>(()=>tree.Delete(null));
+            Assert.ThrowsException<ArgumentNullException>(() => tree.Delete(null));
         }
 
         [TestMethod]
         public void TreeFindNullMustThrowException()
         {
             Tree<string> tree = new Tree<string>();
-            Assert.ThrowsException<ArgumentNullException>(()=>tree.Find(null));
+            Assert.ThrowsException<ArgumentNullException>(() => tree.Find(null));
         }
 
         [TestMethod]
         public void TreeContainsNullMustThrowException()
         {
             Tree<string> tree = new Tree<string>();
-            Assert.ThrowsException<ArgumentNullException>(()=>tree.Contains(null));
+            Assert.ThrowsException<ArgumentNullException>(() => tree.Contains(null));
         }
     }
 }
